9db65a52259c6873c1af3ec1a826ba1dd0862a13
João Victor <joao.victor_b@hotmail.com>
2021-11-23 16:27:49 -0300

    Criando menu

--- src/main/java/br/iesb/indicador_analise_grafica/Main.java
+++ src/main/java/br/iesb/indicador_analise_grafica/Main.java
@@ -7,12 +7,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.iesb.indicador_analise_grafica_enum.Perfil;
-
 
 @SpringBootApplication
 public class Main {
@@ -22,25 +22,118 @@ public class Main {
 		SpringApplication.run(Main.class, args);
 		long start = System.currentTimeMillis();
 
-		Perfil perfil1 = Perfil.ARROJADO;
-		Perfil perfil2 = Perfil.MODERADO;
-		Perfil perfil3 = Perfil.CONSERVADOR;
+		Perfil perfil = null;
 		
-		//TreinamentoRedeNeural.realizaTreinamentoProcurandoPadroesEmPapeisOperaveis();
-		//TreinamentoRedeNeural.confereAlvosDasOperacoesPossiveis();
-		//RedeNeural.preenchendoEstatisticaPiercingLine(perfil1);
-		//RedeNeural.preencherTxtValidacaoRedeNeuralPiercingLine();
-		//RedeNeural.realizaTreinamentoRedeNeuralPiercingLine(perfil1);
+		perfil = selecionaPerfil(perfil);
 		
-		//RedeNeural.preenchendoEstatisticaTresSoldados(perfil3);
-		//RedeNeural.preencherTxtValidacaoRedeNeuralTresSoldados();
-		//RedeNeural.realizaTreinamentoRedeNeuralTresSoldados(perfil3);
-		RedeNeural.testaRedeNeuralTresSoldadosNaPratica(perfil3);
-		
-		
-		long elapsedTime = (System.currentTimeMillis() - start);
-		System.out.println("Tempo gasto parar rodar: (segundos) " + elapsedTime / 1000F);
+		menu(perfil);
 
+		long elapsedTime = (System.currentTimeMillis() - start);
+		System.out.println("Tempo gasto para rodar: (segundos) " + elapsedTime / 1000F);
+
+	}
+
+	private static void menu(Perfil perfil) {
+		int opcaoDeMenu = -1;
+		do {
+			apresentarAsOpcoesDeMenu();
+			
+			opcaoDeMenu = leValorInteiroParaSwitch();
+			
+			switch (opcaoDeMenu) {
+			case 1:
+				RedeNeural.testaRedeNeuralMarteloNaPratica(perfil);
+				break;
+			
+			case 2:
+				RedeNeural.testaRedeNeuralEngolfoNaPratica(perfil);
+				break;
+				
+			case 3:
+				RedeNeural.testaRedeNeuralPiercingLineNaPratica(perfil);
+				break;
+				
+			case 4:
+				RedeNeural.testaRedeNeuralTresSoldadosNaPratica(perfil);
+				break;
+				
+			case 5:
+				perfil = selecionaPerfil(perfil);
+				break;
+				
+			default:
+				System.out.println("Opcao Invalida.");
+
+			}
+
+		} while (opcaoDeMenu != 0);
+	}
+
+	private static Perfil selecionaPerfil(Perfil perfil) {
+		int valorDoPerfil = 0;
+		
+		do{
+			opcoesParaPerfilEscritas();
+			valorDoPerfil = leValorInteiroParaSwitch();
+			
+			perfil = switchParaSelecionarPerfil(valorDoPerfil);
+			
+		}while(perfil == null);
+		
+		return perfil;
+		
+	}
+
+	private static Perfil switchParaSelecionarPerfil(int valorDoPerfil) {
+		Perfil perfil;
+		switch (valorDoPerfil) {
+		case 1:
+			perfil = Perfil.ARROJADO;
+			return perfil;
+			
+		case 2:
+			perfil = Perfil.MODERADO;
+			return perfil;
+			
+		case 3:
+			perfil = Perfil.CONSERVADOR;
+			return perfil;
+			
+		default:
+			System.out.println("Opcao Invalida.");
+			return null;
+		}
+	}
+
+	private static int leValorInteiroParaSwitch() {
+		int valorDoPerfil = -1;
+		Scanner scanner = new Scanner(System.in);
+		try {
+			valorDoPerfil = Integer.parseInt(scanner.nextLine());
+		}catch(NumberFormatException numberException) {
+			System.out.println("Não pode selecionar letra.");
+		}
+		return valorDoPerfil;
+	}
+
+	private static void opcoesParaPerfilEscritas() {
+		System.out.println();
+		System.out.println("Digite a Opcao de Perfil: ");
+		System.out.println("1 - Arrojado ");
+		System.out.println("2 - Moderado ");
+		System.out.println("3 - Conservador ");
+		System.out.println();
+	}
+
+	private static void apresentarAsOpcoesDeMenu() {
+		System.out.println();
+		System.out.println("1 - Realiza Treinamento Para O Padrao Martelo.");
+		System.out.println("2 - Realiza Treinamento Para O Padrao Engolfo.");
+		System.out.println("3 - Realiza Treinamento Para O Padrao Piercing Line.");
+		System.out.println("4 - Realiza Treinamento Para O Padrao Tres Soldados.");
+		System.out.println("5 - Troca O Perfil De Usuario.");
+		System.out.println("0 - Sair.");
+		System.out.println();
 	}
 
 }
